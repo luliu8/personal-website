e920ad46e30c2555f7541bf9216587725e32342a
Lu Liu <lljy871224@gmail.com>
2017-11-03 19:01:21 -0700

    setup postgresql database

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,12 @@
 class ProjectsController < ApplicationController
   def index
+    @projects=Project.all
+
   end
 
 
+
   def visual_recommender
+    @project=Project.find(params[:project_id])
   end
 end
