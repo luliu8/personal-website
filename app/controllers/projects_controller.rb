4758255021bf4ec158e7266377b2a7f19d229bb5
Lu Liu <lljy871224@gmail.com>
2018-05-08 22:23:20 -0700

    fix openseadragon zoom

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -12,7 +12,6 @@ class ProjectsController < ApplicationController
   def visual_recommender
     if params[:'books-search-txt']
       @book = Book.find(params[:'books-search-txt']).first
-
       if @book
         @neighbors = Book.find_neighbors(@book.vec)
         respond_to do |format|
@@ -20,7 +19,7 @@ class ProjectsController < ApplicationController
         end
       end
     else
-      gon.zoom_center=-1
+      gon.viewer=nil
     end
   end
 
